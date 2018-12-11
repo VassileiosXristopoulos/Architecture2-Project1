7ae1e73a0a929ba97ed38bf927b04b9bb51f7c23
VassileiosXristopoulos <vpxristop@gmail.com>
2018-12-11 20:00:46 +0200

    All counters fully implemented and tested. Array implementation missing.

--- excercise_i.s
+++ excercise_i.s
@@ -1,17 +1,24 @@
 .data
-A:  .word 0x4008000000000000
-    .word 0x3FEFFFFFFFFFFFFF
-    .word 0x3FE8000000000000
-    .word 0xBFFC000000000000
-
-B:  .space 1200
-C:  .space 1200
 
 CONTROL: .word 0x10000
 DATA:    .word 0x10008
+SPEC_EXP: .word 0x07FF
 
 MAXINT64: .word 0x7FFFFFFFFFFFFFFF
 MININT64: .word 0x8000000000000000
+
+
+A:  .word 	0x4008000000000000 ; 3
+			; 0xBFF4000000000000 ; -1.25
+			; 0xBFD3333333333333 ; -0.3
+			; 0x7FEFFFFFFFFFFFFF ; out of int64 not in [-0.5 , 0.5]
+			; 0x7FF0000000000000 ;infinity
+			; 0x0000000000000000 ; question for the 1 Million. Which number is that?
+		    
+B:  .space 1200
+C:  .space 1200
+
+
 
 .text
 MAIN:
@@ -22,6 +29,7 @@ MAIN:
 
     ld R1, A($zero)                     ; Load the address of A. This is not going to be needed in the final code
     
+    
     ; Counters initialization
     xor R10, R10, R10                   ; This is our counter for (P)
     xor R11, R11, R11                   ; This is our counter for (N)
@@ -30,6 +38,10 @@ MAIN:
     xor R14, R14, R14                   ; This is our counter for (Z)
     xor R15, R15, R15                   ; This is our counter for (I)
 
+    dsll R17, R1, 1			; remove sign to check for zero input
+    beqz  R17, __NUMBER_ZERO		; check if input number is zero
+
+    
     ; This is just for testing cvt behaviour
     ;mtc1 R1, F0
     ;cvt.l.d F0, F0
@@ -51,13 +63,25 @@ MAIN:
     daddui R8, $zero, 1
     daddui R5, $zero, 52                ; the shift amount to get the 1 in front of the mantissa
     dsllv R8, R8, R5                    ; shift 1 to the 53 bit position so we add that to our mantissa.
-    or R4, R4, R8                       ; This is the explicit 1 added to the mantissa in IEEE-754 format
+    
 
     ; Compute exponent
     dsrlv R5, R1, R5                    ; shifting 52 bits to the right to get the exponent to the lowest bits
     andi R5, R5, 0x07FF                 ; and-ing to get only the exponent
-    daddi R5, R5, -1023                 ; subtracting bias
 
+__HANDLE_INFINITY:
+	ld R19, SPEC_EXP($zero)
+    bne R5, R19, __COMPUTE_NUMBER		; check if exponent is all ones (infinity or NaN)
+	bnez R4, __COMPUTE_NUMBER			; if exponent all zeros and mantissa != 0 then continue
+    daddi R15, R15, 1					; else increase counter for infinity numbers
+    j __EXIT
+  
+__COMPUTE_NUMBER:
+	or R4, R4, R8                       ; This is the explicit 1 added to the mantissa in IEEE-754 format
+    ; else continue computing the number
+    daddi R5, R5, -1023                 ; subtract bias
+
+    
     slt R7, R5, $zero                   ; check if exponent is negative
     
     ; zeroing out R25 which is our result register.
@@ -65,7 +89,7 @@ MAIN:
     ; also we put this instruction so as not to have RAW hazard
     xor R25, R25, R25
     
-    bnez R7, __PRINT_VALUE              ; if it is just print the result which is 0
+    bnez R7, __MANIP_LESS_1             ; if exponent negative check if |number| < 0.5 and print  
 
     slti R7, R5, 62                     ; else check if exponent is less or equal to 62
 
@@ -74,6 +98,7 @@ MAIN:
     bnez R7, __COMPUTE_INTEGER          ; if it is then go to compute the integer
     beqz R2, __LOAD_MAXINT              ; else check if sign is zero and if it is load MAXINT
 
+    
 __LOAD_MININT:
     ld R25, MININT64($zero)             ; else load MININT
     daddui R13, R13, 1                  ; increase counter for (T)
@@ -83,6 +108,15 @@ __LOAD_MAXINT:
     ld R25, MAXINT64($zero)
     daddui R13, R13, 1                  ; increase counter for (T)
     j __PRINT_VALUE
+
+__SPEC_CASES:
+    bnez R15, __EXIT		; if exponent all zeros and mantissa != 0 then NaN
+    daddi R15, R15, 1			; else increase counter for infinity numbers
+    j __EXIT
+
+__NUMBER_ZERO:
+   daddi R14, R14, 1			; increase (Z) counter
+   j __EXIT
 
 __COMPUTE_INTEGER:
     slti R7, R5, 52                     ; check if exponent is less or equal to 52
@@ -102,13 +136,19 @@ __SHIFT_MANTISSA_LEFT:
     dsllv R25, R4, R28
 
 __APPLY_SIGN:
-    beqz R2, __INCREASE_N               ; if sign is zero then increase (N) and print the value
-    daddui R10, R10, 1                  ; increase counter for (P)
+    beqz R2, __INCREASE_P               ; if sign is zero then increase (P) and print the value
+    daddui R11, R11, 1                  ; increase counter for (N)
     xor R25, R25, R27                   ; else xor with all ones (that is equavalent to not)
     daddi R25, R25, 1                   ; and add 1 to the result
+	j __PRINT_VALUE
+	
+__INCREASE_P:
+    daddui R10, R10, 1                  ; increase counter for (P)
+	j __PRINT_VALUE
 
-__INCREASE_N:
-    daddui R11, R11, 1                  ; increase counter for (N)
+__MANIP_LESS_1:
+    slti R21, R5, -1 		; check if exp < -1 -- if exp >= -1 then number*2>=1 --> |number| > 0,5
+    daddi R12, R12, 1		; if so then increase D counter
 
 __PRINT_VALUE:
     sd R25, 0(R31)                      ; setting DATA
